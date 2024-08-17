3fd69d81cfae444581d13deac54489fe00a0c1fb
Shekhar7055 <140903282+Shekhar7055@users.noreply.github.com>
2024-08-17 09:50:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function updateClock() {
+    const now = new Date();
+    const second = now.getSeconds();
+    const minute = now.getMinutes();
+    const hour = now.getHours();
+    document.getElementById('hour').style.transform = `rotate(${30 * hour + minute / 2}deg)`;
+    document.getElementById('minute').style.transform = `rotate(${6 * minute}deg)`;
+    document.getElementById('second').style.transform = `rotate(${6 * second}deg)`;
+    console.log(30 * hour + minute / 2)
+}
+setInterval(updateClock, 1000);
+updateClock();
